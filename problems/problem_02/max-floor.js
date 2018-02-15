ab31668afa268e6e694ebaf0b43bc63e94a403c3
patiphannn <patiphann@gmail.com>
2018-02-15 23:08:16 +0700

    Edit code maxFloor

--- problems/problem_02/max-floor.js
+++ problems/problem_02/max-floor.js
@@ -1,10 +1,8 @@
 const maxFloor = (input, loop = 1) => {
-  const diff = input - loop;
-
   if(input < loop) {
   	return loop - 1;
   } else {
-  	return maxFloor(diff, loop + 1);
+  	return maxFloor(input - loop, loop + 1);
   }
 };
 
