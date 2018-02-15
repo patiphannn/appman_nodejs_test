c70c0f1bce3675420954579d60fa53b3529d4035
patiphannn <patiphann@gmail.com>
2018-02-15 23:02:46 +0700

    Fix problem_02

--- problems/problem_02/max-floor.js
+++ problems/problem_02/max-floor.js
@@ -1,5 +1,11 @@
-const maxFloor = input => {
-  // put your code here !!
+const maxFloor = (input, loop = 1) => {
+  const diff = input - loop;
+
+  if(input < loop) {
+  	return loop - 1;
+  } else {
+  	return maxFloor(diff, loop + 1);
+  }
 };
 
 module.exports = { maxFloor };
